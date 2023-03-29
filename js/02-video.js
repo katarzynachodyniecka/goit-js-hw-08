4deadc802ba45a6991d9a8cf62d13da03e3a91ca
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-03-29 11:34:40 +0200

    init 2

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,19 @@
-
+// Zapoznaj się z dokumentacją biblioteki odtwarzacza Vimeo.
+//
+//
+// Dodaj bibliotekę jako zależność projektu poprzez npm.
+//
+//
+// Zainicjalizuj odtwarzacz w pliku skryptu tak, jak opisano w sekcji pre-existing player, ale weź pod uwagę to, że odtwarzacz dodano jako pakiet npm, a nie poprzez CDN.
+//
+//
+// Zbadaj dokumentację metody on() i zacznij śledzić zdarzenie timeupdate - czyli aktualizacje czasu odtwarzania.
+//
+//
+// Zapisuj czas odtwarzania w local storage. Niech kluczem w storage będzie "videoplayer-current-time".
+//
+//
+// Przy przeładowywania strony używaj metody setCurrentTime() aby wznowić odtwarzanie od zapisanego momentu.
+//
+//
+// Dodaj do projektu bibliotekę lodash.throttle i zrób tak, aby czas odtwarzania aktualizował się w storage nie częściej niż raz na sekundę.
--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,11 @@
-
+// Śledź w formularzu zdarzenie input, i za każdym razem zapisuj w local storage obiekt z polami email i message, w których przechowywane są aktualne wartości pól formularza.
+//Niech kluczem do tych danych w storage będzie "feedback-form-state".
+//
+//
+// Podczas przeładowywania strony sprawdzaj stan storage i jeśli są tam zapisane dane, wypełniaj nimi pola formularza. W przeciwnym wypadku pola powinny być puste.
+//
+//
+// Po wysłaniu formularza wyczyść storage i pola formularza, a także wyloguj obiekt z polami email, message i ich aktualnymi wartościami do konsoli.
+//
+//
+// Zrób tak, aby storage aktualizował się nie częściej niż raz na 500 milisekund. Aby to zrobić, użyj metody biblioteki lodash.throttle (dodaj ją do projektu).
