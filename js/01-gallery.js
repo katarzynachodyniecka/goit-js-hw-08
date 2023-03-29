5c5b4e68421ca376e3c5e77d4b9a2ec100474f52
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-03-29 11:27:09 +0200

    # task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.innerHTML = "";
+for (const img of galleryItems) {
+  const newImage = `<a class="gallery__link" href="${img.original}">
+<img
+  class="gallery__image"
+  src="${img.preview}"
+  data-source="${img.original}"
+  alt="${img.description}"
+/>
+</a>`;
+  galleryEl.innerHTML += newImage;
+}
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "outside",
+  captionDelay: 200,
+});
