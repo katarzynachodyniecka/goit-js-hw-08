526a66e7560d5416cd59e1e6dac4e98cc23e5188
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-03-30 14:43:35 +0200

    # task 2

--- js/02-video.js
+++ js/02-video.js
@@ -1,14 +1,18 @@
-// Zapoznaj się z dokumentacją biblioteki odtwarzacza Vimeo.
-//
-//
-// Dodaj bibliotekę jako zależność projektu poprzez npm.
-//
-//
+import Player from "@vimeo/player";
+import _ from "lodash";
 // Zainicjalizuj odtwarzacz w pliku skryptu tak, jak opisano w sekcji pre-existing player, ale weź pod uwagę to, że odtwarzacz dodano jako pakiet npm, a nie poprzez CDN.
 //
 //
 // Zbadaj dokumentację metody on() i zacznij śledzić zdarzenie timeupdate - czyli aktualizacje czasu odtwarzania.
 //
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+const timeCurr = "videoplayer-current-time";
+
+const updateTime = throttle(
+  (data) => localStorage.setItem(timeCurr, Math.floor(timeEl.seconds)),
+  1000
+);
 //
 // Zapisuj czas odtwarzania w local storage. Niech kluczem w storage będzie "videoplayer-current-time".
 //
@@ -17,3 +21,5 @@
 //
 //
 // Dodaj do projektu bibliotekę lodash.throttle i zrób tak, aby czas odtwarzania aktualizował się w storage nie częściej niż raz na sekundę.
+player.setCurrentTime(localStorage.getItem(timeCurr));
+player.on("timeupdate", updateTime);
